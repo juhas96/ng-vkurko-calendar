f01408b0f8228970427619fad208a1f580c91c92
Gonzalo Ruiz <rgon@rgon.es>
2024-09-18 03:25:05 +0200

    port to create-svelte, refactor as single project

---
+++ src/lib/index.ts
@@ -0,0 +1,12 @@
+// Visualization Options
+export { default as ResourceTimeGrid } from './resource-time-grid/index.js'
+export { default as ResourceTimeline } from './resource-timeline/index.js'
+export { default as TimeGrid } from './time-grid/index.js'
+export { default as List } from './list/index.js'
+export { default as DayGrid } from './day-grid/index.js'
+
+// Interaction
+export { default as Interaction } from './interaction/index.js'
+
+// Core
+export { default as Calendar } from './core/index.js'
